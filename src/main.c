1bb2bae2590b65f61b3be5521cd65d9e5f597d0e
Dmitry K <dimka.rs@gmail.com>
2019-06-30 22:22:27 +0300

    Initial

---
+++ src/main.c
@@ -0,0 +1,68 @@
+/* Includes ------------------------------------------------------------------*/
+#include "stm8s.h"
+#include "main.h"
+
+/* Private defines -----------------------------------------------------------*/
+
+/* Private function prototypes -----------------------------------------------*/
+
+/* Private functions ---------------------------------------------------------*/
+
+void main(void)
+{
+
+    while (1)
+    {
+
+    }
+}
+
+
+
+/***********************************************************************/
+
+/* Interrupt example
+void tim4_isr(void) __interrupt(ITC_IRQ_TIM4_OVF) {
+
+}
+*/
+
+
+/* Debug output
+
+int putchar (int c)
+{
+    // this implementation is SDCC 3.8 specific
+    // see sdcc stdio.h for details
+    UART1->DR = (uint8_t) c;
+    while (!(UART1->SR & UART1_FLAG_TXE));
+    return c;
+}
+*/
+
+// This is called by some of the SPL files on error.
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *   where the assert_param error has occurred.
+  * @param file: pointer to the source file name
+  * @param line: assert_param error line source number
+  * @retval : None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  (void)file;
+  (void)line;
+
+  /* User can add their own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
